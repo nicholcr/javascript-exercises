6b1414b3ad55688468cbb49f9328b07d64860052
nicholcr <73195431+nicholcr@users.noreply.github.com>
2025-07-07 17:48:28 -0500

    completed exercises 06 and 07

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, timesRepeated) {
+    let returnString = "";
+    if (timesRepeated >= 0) {
+        for (i = 0; i < timesRepeated; i++) {
+            returnString += string;
+        }
+        return returnString;
+    } else {
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    return str
+    .split("")
+    .map(
+        (letter, index, fullWord) => letter = fullWord[str.length - (index + 1)]
+    )
+    .join('');
 };
 
 // Do not edit below this line
