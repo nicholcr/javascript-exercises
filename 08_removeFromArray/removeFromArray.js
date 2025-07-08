520a74918cd792f2f5b48ac6af58f91a39b29e2f
nicholcr <73195431+nicholcr@users.noreply.github.com>
2025-07-08 13:06:18 -0500

    solved exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elements) {
+    let result = arr;
+    for (const element of elements) {
+        result = result.filter(item => (item !== element))
+    }
+    return result;
 };
 
 // Do not edit below this line
