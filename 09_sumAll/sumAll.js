d0ed79f46d5eac569673948b09b91821a170ecf9
nicholcr <73195431+nicholcr@users.noreply.github.com>
2025-07-08 15:20:51 -0500

    solved exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    if (a < 0 || b < 0 ||
+        !Number.isInteger(a) || !Number.isInteger(b)) {
+        return 'ERROR';
+    }
 
+    let arr = [];
+    let smallerNumber = Math.min(a, b);
+    let largerNumber = Math.max(a, b);
+
+    for (let i = smallerNumber; i <= largerNumber; i++) {
+        arr[i] = i
+    }
+    return arr.reduce((sum, current) => sum + current, 0)
 };
 
 // Do not edit below this line
